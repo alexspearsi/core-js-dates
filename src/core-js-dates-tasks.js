7b514aae422e0a65c15d5533446c322ec6f2784d
Alex <strelchenko.aleksandr@yandex.ru>
2025-03-19 10:46:37 +0200

    feat: complete the 8th task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -231,8 +231,28 @@ function getNextFridayThe13th(/* date */) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  const month = date.getMonth();
+  switch (month) {
+    case 0:
+    case 1:
+    case 2:
+      return 1;
+    case 3:
+    case 4:
+    case 5:
+      return 2;
+    case 6:
+    case 7:
+    case 8:
+      return 3;
+    case 9:
+    case 10:
+    case 11:
+      return 4;
+    default:
+      return null;
+  }
 }
 
 /**
