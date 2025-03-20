47d3e195428ce47bedb22ee818881a1d5a0c63a0
Alex <strelchenko.aleksandr@yandex.ru>
2025-03-20 09:13:38 +0200

    feat: complete the 12th task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -314,8 +314,19 @@ function getWeekNumberByDate(/* date */) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const year = date.getFullYear();
+  const month = date.getMonth();
+
+  for (let i = 0; i <= 11 - month; i += 1) {
+    const newDate = new Date(year, i, 13);
+
+    if (newDate.getDay() === 5) {
+      return newDate;
+    }
+  }
+
+  return false;
 }
 
 /**
